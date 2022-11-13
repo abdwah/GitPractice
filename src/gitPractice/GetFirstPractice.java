09d1ad548f7501dc4bc5e1f9cd8bd31fc7d396a0
unknown <Abdul Wahid Kohgadai>
2022-11-13 00:11:22 -0500

    second commit for remote

--- src/gitPractice/GetFirstPractice.java
+++ src/gitPractice/GetFirstPractice.java
@@ -1,11 +1,10 @@
 package gitPractice;
 
-public class FirstClassGit {
+public class GetFirstPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is test");
-		
+
 	}
 
 }
